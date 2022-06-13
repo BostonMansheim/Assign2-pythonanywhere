3496c36d3c9ab1ce1c0397bd25dd82904481e002
svlasnik-uno <54107634+svlasnik-uno@users.noreply.github.com>
2022-06-13 16:56:13 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -132,7 +132,7 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # from: https://stackoverflow.com/questions/36760549/python-django-youre-using-the-staticfiles-app-without-having-set-the-static-ro
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
