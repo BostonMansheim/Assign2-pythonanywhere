7aa01678cbe5e64cac8b21f6f6070ce41da50f5d
svlasnik-uno <54107634+svlasnik-uno@users.noreply.github.com>
2022-06-13 16:21:38 -0500

    Update settings.py
    one more time..

--- settings.py
+++ settings.py
@@ -131,8 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
