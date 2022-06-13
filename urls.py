4df627fc1202601e60bd6f23a19902b4337fb295
svlasnik-uno <54107634+svlasnik-uno@users.noreply.github.com>
2022-06-13 16:44:57 -0500

    Update urls.py
    added last line

--- urls.py
+++ urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('crm.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
