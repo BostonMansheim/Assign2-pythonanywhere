7cea42c049af5ddc7d3304af043cb04e9350f3de
BostonMansheim <106132026+BostonMansheim@users.noreply.github.com>
2022-06-14 13:13:22 -0500

    commit

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -131,5 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-#if not DEBUG:
-#    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
